de36d8fb947add66b44edae2210e6b8f8dad0cd5
Geeta Todmal <geeta.todmal@vmvinfotech.com>
2026-06-09 05:44:39 -0400

    Update CORS policy

--- src/main/java/com/example/task_trackermysql/TaskController.java
+++ src/main/java/com/example/task_trackermysql/TaskController.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class TaskController {
 
     @Autowired
